beaf64a63728d14c905d2e4ee6758a75ad5e1b4c
pkeiko <paula.keiko.m@gmail.com>
2019-04-20 10:37:05 -0300

    modificações apontadas depois do primeiro relatório Adição de dependências, true price não negativo, preço mais real, áreas mais reais

--- src/scripts/gendata/data_generator.py
+++ src/scripts/gendata/data_generator.py
@@ -7,35 +7,53 @@ data_real_state = pd.DataFrame()
 # set random seed
 np.random.seed(0)
 
-data_real_state['n_dorms'] = np.random.poisson(2, 100000)
+data_real_state['n_dorms'] = (np.random.poisson(4, 100000)//2)
 
 data_real_state['n_suites'] = data_real_state.n_dorms.apply(lambda x:
                                     np.random.randint(x + 1) if x !=0 else 0)
 
 data_real_state['n_bathrooms'] = data_real_state.n_suites.apply(lambda x:
-                                    x + np.random.randint(3) if x != 0
-                                    else np.random.randint(1, 3))
+                                    x + 1 + np.random.randint(2) if x != 0
+                                    else np.random.randint(1, 2))
 
 # n_dorms == 0 -> n_bathrooms == 1
 data_real_state.loc[data_real_state.n_dorms == 0, 'n_bathrooms'] = 1
 
-data_real_state['area'] = data_real_state.n_dorms.apply(lambda x:
-                                    np.random.normal(25, 10) if x == 0
-                                    else(np.random.normal(40, 10) if x == 1
-                                    else np.random.normal(50, 10) if x == 2
-                                    else np.random.normal(70, 15)))
-
 # 1 - apartment, 0 - house
-data_real_state['real_state_type'] = np.random.randint(2, size=100000)
+#supposing apartments are likely to have less bedrooms
+data_real_state['real_state_type'] = data_real_state.n_dorms.apply(lambda x:
+                                        0 if np.random.randint(x-2,x+5)/3 > 1 else 1 )
 
-# 1 - there is garage, 0 - there isn't garare
-data_real_state['flag_garage'] = np.random.randint(2, size=100000)
+# 1 - there is garage, 0 - there isn't garage
+#supposing houses are more likely to have garage
+#def garage(type):
+#    if (type == 0):
+#        if (np.random.randint(100) < 85):
+#            return 1
+#    else: #if type == 1
+#        if(np.random.randint(10) < 7):
+#            return 1
+#    return 0
+
+def garage(type, ndorm):
+    if (type == 0):
+        if (np.random.randint(100) < (ndorm+1)*25):
+            return 1
+    else: #if type == 1
+        if(np.random.randint(100) < (ndorm+1)*20):
+            return 1
+    return 0
+
+data_real_state['flag_garage'] = data_real_state.apply(lambda row: garage(row['real_state_type'], row['n_dorms']),axis = 1)
+#data_real_state['flag_garage'] = data_real_state.real_state_type.apply(lambda x: garage(x))
+
+#                                    1 if np.random.randint(100) < 85  and x == 0
+#                                    else np.random.randint(100) < 70 )  
 
 # 1 - yes, 0 - no
 data_real_state['near_subway'] = np.random.randint(2, size=100000)
 
 # 1 - yes, 0 - no
-#data_real_state['concierge_service'] = np.random.randint(2, size=100000)
 data_real_state['concierge_service'] = data_real_state.real_state_type.apply(lambda x: np.random.randint(2) if x!=0 else 0)
 
 # 1 - yes, 0 - no
@@ -43,19 +61,26 @@ data_real_state['furnished'] = np.random.randint(2, size=100000)
 
 data_real_state['age'] = np.random.uniform(0, 45, 100000)
 
+data_real_state['area'] = data_real_state.n_dorms.apply(lambda x: 
+                            np.random.normal(55*(x+1),5))
+
+#                                    np.random.normal(25, 5) if x == 0
+#                                    else(np.random.normal(40, 5) if x == 1
+#                                    else np.random.normal(50, 5) if x == 2
+#                                    else np.random.normal(70, 5)))
+
 # creating true_price using weighted sum + bayesian error
-data_real_state['true_price'] = 1000 + (200 * data_real_state['n_dorms'] +
-                                15 * data_real_state['n_suites'] +
+data_real_state['true_price'] = 100+(100 * data_real_state['n_dorms'] +
+                                125 * data_real_state['n_suites'] +
                                 50 * data_real_state['n_bathrooms'] +
-                                5 * data_real_state['area'] +
-                                100 * data_real_state['real_state_type'] +
-                                150 * data_real_state['flag_garage'] +
+                                30 * data_real_state['real_state_type'] +
+                                50 * data_real_state['flag_garage'] +
                                 200 * data_real_state['near_subway'] +
-                                100 * data_real_state['concierge_service'] +
-                                100 * data_real_state['furnished'] +
-                                (-30) * data_real_state['age'] +
-                                np.random.normal(0, 200)
-                                )
+                                50 * data_real_state['concierge_service'] +
+                                150 * data_real_state['furnished'] +
+                                (-15) * data_real_state['age'] +
+                                np.random.randint(20,40)*data_real_state['area'])
+                                
 # creating price using price + variation
 data_real_state['price'] = (((np.random.normal(0, 0.1, 100000) + 1) *
                             data_real_state['true_price']))
